7f3bc88a0e4a700c9b618372a02e7b91131e3b4c
Ilya Chudakov <ilyuhi4@yandex.ru>
2022-01-10 02:27:36 +0300

    before deploy on server

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
+from django.contrib.auth import authenticate, login
 from django.core.mail import send_mail
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
-from .forms import EmailPostForm
+from django.db.models import Count
+
+from taggit.models import Tag
+
+from .forms import EmailPostForm, CommentForm, LoginForm
 from .models import Post
 
 
@@ -21,28 +26,59 @@ def first_index(request):
     return HttpResponse('<h1>Hello, world!!!</h1>')
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-    # If page is not an integer deliver the first page
+        # If page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
-    # If page is out of range deliver last page of results
+        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+    return render(request, 'blog/post/list.html',
+                  {'page': page, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post,
-                             status='published',
+    post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
@@ -50,13 +86,13 @@ def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
     sent = False
     if request.method == 'POST':
-    # Form was submitted
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-        # Form fields passed validation
+            # Form fields passed validation
             cd = form.cleaned_data
             post_url = request.build_absolute_uri(
-            post.get_absolute_url())
+                post.get_absolute_url())
             subject = f'{cd["name"]} recommends you read ' \
                       f'\"{post.title}\"'
             print(subject)
@@ -68,3 +104,24 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html',
                   {'post': post, 'form': form, 'sent': sent})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request,
+                                username=data['username'],
+                                password=data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated succefully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return ('Invalid login')
+        else:
+            form = LoginForm()
+        return render(request, 'accounts/login.html', {'form': form})
