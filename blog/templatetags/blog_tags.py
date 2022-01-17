04f60e87e1cbf26dfb93957275e3eb962be92877
Ilya Chudakov <ilyuhi4@yandex.ru>
2022-01-17 03:23:39 +0300

    Create new tag that returns vocabulary and render it by decorator

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -10,3 +10,8 @@ def total_posts():
        To use it you need to {% load %} in template
     """
     return Post.published.count()
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by(field_names='-publish')[:count]
+    return {'latest_posts': latest_posts}
