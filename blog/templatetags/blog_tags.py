51f76e94496fdbfb06c4a93db4c4a914bdbb5da0
Ilya Chudakov <ilyuhi4@yandex.ru>
2022-01-17 03:45:56 +0300

    add markdown filter to tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,9 +1,15 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 from ..models import Post
 
 register = template.Library()
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 @register.simple_tag
 def total_posts():
